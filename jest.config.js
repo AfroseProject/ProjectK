49f8748a5a65bc91afce0c08370c0572d2fa504e
AfroseProject <afroseprojectbiz@gmail.com>
2026-07-14 20:00:28 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,23 @@ module.exports = {
   preset: 'react-native',
   testEnvironment: 'node',
 
-  transformIgnorePatterns: [
-    'node_modules/(?!(@react-native|react-native|@react-native-firebase|@react-navigation|react-native-safe-area-context|@shopify/flash-list|react-native-maps)/)',
-  ],
-
   setupFilesAfterEnv: [
     '<rootDir>/jestSetup.js',
     '@testing-library/jest-native/extend-expect',
+  ],
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(' +
+      [
+        '@react-native',
+        '@react-native-firebase',
+        '@react-navigation',
+        'react-native',
+        'react-native-safe-area-context',
+        'react-native-screens',
+        'react-native-gesture-handler',
+        '@shopify/flash-list',
+      ].join('|') +
+      ')/)',
   ],
 };
